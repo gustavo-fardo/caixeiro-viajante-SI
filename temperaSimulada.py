bec656dc890e57cec704de3e65822b1f3d55f22d
gustavo-fardo <gustavo.fardo@hotmail.com>
2024-03-27 09:57:35 -0300

    ajuste para contar o retorno e multiplas iteracoes

--- temperaSimulada.py
+++ temperaSimulada.py
@@ -28,27 +28,27 @@ def gera_solucao(n_cidades):
     sol = np.random.permutation(n_cidades)
     return np.append(sol, sol[0])
 
-def calcula_custo(cidades, n_cidades, solucao):
+def calcula_custo(cidades, solucao):
     custo = 0
-    for i in range(1, n_cidades):
+    for i in range(1, len(solucao)):
         custo+=cidades[solucao[i]][solucao[i-1]]
     return custo
 
 def retornaTemp(t, max_iter):
     return 100*((max_iter-t)/max_iter)**2
 
-def temperaSimulada(cidades, n_cidades, sol_inicial, max_iter):
+def temperaSimulada(cidades, sol_inicial, max_iter):
     solucao_atual = sol_inicial
-    custo_atual = calcula_custo(cidades, n_cidades, solucao_atual)
+    custo_atual = calcula_custo(cidades, solucao_atual)
     for t in range(max_iter):
         T = retornaTemp(t, max_iter)
         if T == 0:
             return solucao_atual
-        prox_solucao = gera_solucao(n_cidades)
+        prox_solucao = gera_solucao(np.shape(cidades)[0])
         # print(f"solucao atual:{solucao_atual}")
         print(f"custo atual:{custo_atual}")
         # print(f"solucao vizinha:{prox_solucao}")
-        prox_custo = calcula_custo(cidades, n_cidades, prox_solucao)
+        prox_custo = calcula_custo(cidades, prox_solucao)
         print(f"custo vizinho:{prox_custo}")
         delta_custos = custo_atual - prox_custo
         print(f"delta:{delta_custos}")
@@ -66,14 +66,14 @@ def temperaSimulada(cidades, n_cidades, sol_inicial, max_iter):
     return solucao_atual
 
 n_cidades = 20
-
 cidades = cria_cidades(n_cidades)
-
 s = gera_solucao(n_cidades)
 print(s)
-custo = calcula_custo(cidades, n_cidades, s)
-print(custo)
 
-sf = temperaSimulada(cidades, n_cidades, s, max_iter=100)
-print(sf)
-print(calcula_custo(cidades, n_cidades, sf))
+inp = None
+while inp != "exit":
+    inp = input("'exit' para sair")
+
+    sf = temperaSimulada(cidades, s, max_iter=1000)
+    print(sf)
+    print(calcula_custo(cidades, sf))
